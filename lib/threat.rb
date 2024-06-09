f7a7eb25672fe01945b9c6a8c3e19749e11213f6
Egor Iskrenkov <egor@iskrenkov.me>
2024-06-09 20:41:01 +0200

    Declare module first

--- lib/threat.rb
+++ lib/threat.rb
@@ -3,9 +3,9 @@
 require 'zeitwerk'
 require 'danger'
 
+module Threat
+end
+
 loader = Zeitwerk::Loader.for_gem
 loader.setup
 loader.eager_load_namespace(Threat::Plugins) # This way we're autoloading all defined plugins for Danger to register
-
-module Threat
-end
